2c79a3475b6ca40edef4880e86ccc837163e5992
ajie <xxx@qq.com>
2024-03-21 09:22:03 +0800

    pageHelper 分页插件使用 --- xml

--- class04-MybatisDemo-Annotation/src/main/java/com/mybatis/test/PluginTest.java
+++ class04-MybatisDemo-Annotation/src/main/java/com/mybatis/test/PluginTest.java
@@ -1,5 +1,7 @@
 package com.mybatis.test;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.mybatis.domain.User;
 import com.mybatis.mapper.IUserMapper;
 import org.apache.ibatis.io.Resources;
@@ -31,6 +33,27 @@ public class PluginTest {
         for (User user : users) {
             System.out.println(user);
         }
+    }
+
+    @Test
+    public void testPageHelper() {
+        // 设置分⻚参数
+        PageHelper.startPage(1, 2);
+        // 分页查询会执行两次 sql
+        // 1. 查询总记录条数
+        // 2. 查询 limit 下的数据
+        List<User> users = userMapper.findAllUser();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        PageInfo<User> pageInfo = new PageInfo<>(users);
+        System.out.println("总条数：" + pageInfo.getTotal());
+        System.out.println("总⻚数：" + pageInfo.getPages());
+        System.out.println("当前⻚：" + pageInfo.getPageNum());
+        System.out.println("每⻚显万⻓度：" + pageInfo.getPageSize());
+        System.out.println("是否第⼀⻚：" + pageInfo.isIsFirstPage());
+        System.out.println("是否最后⼀⻚：" + pageInfo.isIsLastPage());
     }
 
 }
